68d9e6ed09320a2eb78752e9054e9730274f0b3a
Angelina <linaemalmros@gmail.com>
2023-12-27 13:43:00 +0100

    added repository

---
+++ src/main/java/com/example/buslines/repository/BusRepository.java
@@ -0,0 +1,9 @@
+package com.example.buslines.repository;
+
+import com.example.buslines.model.Bus;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BusRepository extends JpaRepository<Bus, Integer> {
+}
