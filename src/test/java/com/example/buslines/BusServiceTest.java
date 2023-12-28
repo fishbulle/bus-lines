377c0ce344a5b67e28028e5127231bb151ca7fb8
Angelina <linaemalmros@gmail.com>
2023-12-28 11:48:55 +0100

    removed previous test because of project structure changes

---
+++ src/test/java/com/example/buslines/BusServiceTest.java
@@ -0,0 +1,8 @@
+package com.example.buslines;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class BusServiceTest {
+
+}
