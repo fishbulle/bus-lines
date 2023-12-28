41bb3d5b69dcfe3523021321824b1e5a92a30d78
Angelina <linaemalmros@gmail.com>
2023-12-28 14:26:51 +0100

    added method for getting top 10 buses

--- src/main/java/com/example/buslines/controller/BusController.java
+++ src/main/java/com/example/buslines/controller/BusController.java
@@ -1,15 +1,30 @@
 package com.example.buslines.controller;
 
+import com.example.buslines.apiClient.ApiService;
+import com.example.buslines.model.BusEntity;
 import com.example.buslines.service.BusService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 
 @RestController
-@RequestMapping("api/v1/bus")
+@RequestMapping("/api/v1/bus")
 public class BusController {
 
+    @Autowired
+    private ApiService apiService;
+
+    @Autowired
     private BusService busService;
 
-
+    @GetMapping("/top")
+    public ResponseEntity<List<BusEntity>> getTopBusesWithMostStops(@RequestParam(defaultValue = "10") int limit) {
+        List<BusEntity> topBuses = busService.getTopBusesWithMostStops(limit);
+        return ResponseEntity.ok(topBuses);
+    }
 }
--- src/main/java/com/example/buslines/service/BusService.java
+++ src/main/java/com/example/buslines/service/BusService.java
@@ -1,21 +1,31 @@
 package com.example.buslines.service;
 
-import org.springframework.beans.factory.annotation.Value;
+import com.example.buslines.dto.StopPointDTO;
+import com.example.buslines.model.BusEntity;
+import com.example.buslines.model.StopEntity;
+import com.example.buslines.repository.BusRepository;
+import com.example.buslines.repository.StopRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class BusService {
 
-    @Value("${application.baseUrl}")
-    private String baseUrl;
+    @Autowired
+    private BusRepository busRepository;
 
-    @Value("${application.stopPointUrl}")
-    private String stopPointUrl;
+    @Autowired
+    private StopRepository stopRepository;
 
-    @Value("${application.lineUrl}")
-    private String lineUrl;
-
-    @Value("${application.journeyPatternUrl}")
-    private String journeyPatternUrl;
-
+    public List<BusEntity> getTopBusesWithMostStops(int limit) {
+        List<BusEntity> topBuses = busRepository.findAll();
+        topBuses.sort(Comparator.comparingInt(bus -> bus.getStops().size()));
+        Collections.reverse(topBuses);
+        return topBuses.stream().limit(limit).collect(Collectors.toList());
+    }
 }
