329585063eca2b5d657f5781fc46873db559190e
Angelina <linaemalmros@gmail.com>
2023-12-28 14:23:32 +0100

    added queries

--- src/main/java/com/example/buslines/repository/BusRepository.java
+++ src/main/java/com/example/buslines/repository/BusRepository.java
@@ -4,6 +4,15 @@ import com.example.buslines.model.BusEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface BusRepository extends JpaRepository<BusEntity, Long> {
+
+    Optional<BusEntity> findByLineNumber(String lineNumber);
+
+    interface TopBusResult {
+        String getBusNumber();
+        Integer getStopCounts();
+    }
 }
--- src/main/java/com/example/buslines/repository/StopRepository.java
+++ src/main/java/com/example/buslines/repository/StopRepository.java
@@ -4,6 +4,12 @@ import com.example.buslines.model.StopEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface StopRepository extends JpaRepository<StopEntity, Long> {
+    Optional<StopEntity> findByStopPointNumber(String stopPointNumber);
+
+    Optional<StopEntity> findByStopPointName(String stopPointName);
 }
