af0509eb81a91e8f8e2b1660b160207dd5916369
Angelina Malmros <linaemalmros@gmail.com>
2024-01-03 15:10:00 +0100

    added cors config

---
+++ src/main/java/com/example/buslines/config/CorsConfig.java
@@ -0,0 +1,23 @@
+package com.example.buslines.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(
+                        "https://localhost:5173",
+                        "http://localhost:5173",
+                        "https://www.localhost:5173",
+                        "http://www.localhost:5173"
+                )
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .maxAge(3600);
+    }
+}
